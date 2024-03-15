316fc2a925ffd54f1870f6a2ae175395185462db
Noah <kramerfm@gmail.com>
2024-03-14 20:04:46 -0700

    updates

--- enuf.py
+++ enuf.py
@@ -493,9 +493,8 @@ class ChatBotHandler:
 
         # Calculate respond probability
         x = self.message_counter[msg.room.name]
-        const_term = np.exp(-1 / 5)
-        respond_probability = (np.exp((x - max_messages - 1) / 5) - const_term) / (
-                const_term - np.exp((1 - max_messages - 1) / 5))
+        respond_probability = (np.exp((x - (max_messages + 1)) / 5) - np.exp((1 - (max_messages + 1)) / 5)) / (
+            np.exp((max_messages - (max_messages + 1)) / 5) - np.exp((1 - (max_messages + 1)) / 5))
         print_line(f'Respond probability in {msg.room.name}: {round(respond_probability * 100)}%', 2)
 
         # Generate a response if random value is less than respond probability
